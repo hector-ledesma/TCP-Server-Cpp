7f93d7a240c4043ff1cbc014de810564163e2db7
Hector <hectoremil21@hotmail.com>
2021-10-27 14:55:31 -0400

    Quote of the day tutorial code.

--- ListenerProject/main.cpp
+++ ListenerProject/main.cpp
@@ -2,20 +2,30 @@
 #include <string>
 
 #include "TCPListener.cpp"
+#include "Qotd.h"
 
 void Listener_MessageReceived(CTcpListener* listener, int client, std::string msg);
 
+CQotd quotes("wisdom.txt");
+
 void main()
 {
+
+	
+
 	CTcpListener server("127.0.0.1", 54010, Listener_MessageReceived);
 
 	if (server.Init())
 	{
 		server.Run();
 	}
+
 }
 
 void Listener_MessageReceived(CTcpListener* listener, int client, std::string msg)
 {
-	listener->Send(client, msg);
+	if (msg == "QUOTE")
+	{
+		listener->Send(client, quotes.GetRandomQuote());
+	}
 }
